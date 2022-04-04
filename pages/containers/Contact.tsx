ce8966a42bc7ba8e1cc5098b38696902673179a7
Timo Heman <timoheman16@gmail.com>
2022-04-04 23:05:28 +0100

    add contact animation

--- pages/containers/Contact.tsx
+++ pages/containers/Contact.tsx
@@ -1,29 +1,68 @@
+import { useAnimation, motion } from 'framer-motion'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
 import Section from '../components/Section'
 
 function Contact() {
+  const { ref, inView } = useInView({ threshold: 0.5 })
+  const header = useAnimation()
+  const text = useAnimation()
+  const btn = useAnimation()
+
+  const sequenceIn = async () => {
+    await header.start({ y: 0, opacity: 1 })
+    await text.start({ x: 0, opacity: 1 })
+    btn.start({ opacity: 1 })
+  }
+
+  const sequenceOut = async () => {
+    header.start({ y: -100, opacity: 0 })
+    text.start({ x: -100, opacity: 0 })
+    btn.start({ opacity: 0 })
+  }
+
+  useEffect(() => {
+    if (inView) {
+      sequenceIn()
+    } else {
+      sequenceOut()
+    }
+  }, [inView])
+
   return (
     <>
-      <Section title="Contact" num="04" id="contact">
+      <Section title="Contact" num="04" id="contact" el={ref}>
         <div className="mx-auto mt-16 w-3/4 text-center md:w-1/2 ">
-          <h2 className="mb-4 text-3xl font-black uppercase">
+          <motion.h2
+            initial={{ y: -100, opacity: 0 }}
+            animate={header}
+            className="mb-4 text-3xl font-black uppercase"
+          >
             Let's work together!
-          </h2>
-          <p className="mb-8">
+          </motion.h2>
+          <motion.p
+            initial={{ x: -100, opacity: 0 }}
+            animate={text}
+            className="mb-8"
+          >
             Looking for a front end dev for your team? You can reach out to me
             via email by clicking the button below. I'm also open to making
             friends and meeeting awesome people. Be sure to check out my socials
             below.
-          </p>
+          </motion.p>
           <Link href="mailto:timoheman16@gmail.com">
             <a>
-              <button
+              <motion.button
+                initial={{ opacity: 0 }}
+                animate={btn}
+                whileHover={{ scale: 1.1, y: -2 }}
+                whileTap={{ y: 2 }}
                 type="button"
                 className="mx-auto mt-8 cursor-pointer rounded-full bg-gradient-to-r from-primary to-secondary px-8 py-4 text-2xl uppercase text-white"
               >
                 Work With Me
-              </button>
+              </motion.button>
             </a>
           </Link>
         </div>
